05125b95dc484aecfa0208aeac95767419fc25a2
akarac <adnan@p3rsonal.com>
2013-01-21 17:01:43 +0100

    added file reader and conversion of CG strings to map

--- src/problems/GCContent.scala
+++ src/problems/GCContent.scala
@@ -1,15 +1,6 @@
 package problems
 
-/*
- * The GC-content of a DNA string is given by the percentage of symbols in the string that are 'C' or 'G'. For example, the GC-content of "AGCTATAG" is 37.5%. 
- * Note that the reverse complement of any DNA string has the same GC-content.
- * DNA strings must be labeled when they are consolidated into a database. A commonly used method of string labeling is called FASTA format. 
- * In this format, the string is introduced by a line that begins with '>', followed by some labeling information. 
- * Subsequent lines contain the string itself; the first line to begin with '>' indicates the label of the next string. 
- * In Rosalind's implementation, a string in FASTA format will be labeled by the ID "Rosalind_xxxx", where "xxxx" denotes a four-digit code between 0000 and 9999.
- */
-
-class GCContent {
+object GCContent extends App {
 	def gcPercent(dna: String): String = {
 	  val dnaList: List[Char] = dna.toList
 	  ((gcPercent0(dnaList, 0) / dnaList.length) * 100).toString().concat("%")
@@ -24,4 +15,6 @@ class GCContent {
 	    case Nil => acc
 	  }
 	}
+	
+	println(gcPercent("CAGGCGAAGTTCGGGACTGTTATTCCACTTAGCGTAGCCGACCCAGCCCAGTCTTTATCGTGCATCCCTCTAACCCGCGCATGGGGGGGAACATTGAAAAATTGCACAACTAGCCGTACGCGTAAAGTCACGCCGCTCGCTATGATTCGTTCAATTAGACACGTTATGTCAAAAACTTCTGGCACCTCCTCGAAAGGGGATCCTCAATTAAGTCCAAATTGGTTACGCGCAGTGATGATTGTTTCTGGCAGGGGAAGTTAGGGCGATGGACATAGCCGCGGCGGCGGGGTTCCCCAGAGTCAACAGCTGGAATGGAACACTACGCAACCTCCATTACTTCATCTCTGACTCTTCCTAAGTGATTTAGACATGAAGATATAATTACTGGGAGGAATTGCACATGGTTTTTCCGTCGGCAAGAACTATCTGTCAAAAAATAGGCCCGAATCGCCTGTGTTGCGTGTTAGACTACGCCTCAGTGACTCTAGAAATTTTATTAAGCTGAATGCCAAGCGCAATGCGACGCTAGTAAAAGTGCTGGCTACCGCGATACACCTTACCCGTCTGGTCTACTTTCACGGGCCGAGGTGTCGAGGTTAGTCCTTTTAATCGTCGCCCACACAGCATATCTTATAGGAGCACGGCTCACAATCATGTGACGAGCTTCCACGGCGATAGCGCAAAACATAGACCTTTTGATCGTACTCGTGGACTAGAATCTGTAGTAGACTACACCAGGAATCCATGGTAAGTATTTCAACATTTCGACTCATGGATTAGGGCACCGATACAGCACGAAACTCTAATTCTCCAATTGCTTAATTCCACAGTACCGTGTCACGTCTGTATAATTAGGTGTTGTACATATTCGCATAATTAGCTGGCTGGCCCGTATATTGAATGTA"))
 }
